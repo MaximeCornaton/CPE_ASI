3e8080dceddbfb05f7909146525e494a65ecead0
valbrqd <valbrqd@gmail.com>
2023-05-03 16:49:32 +0200

    achatvente

---
+++ Atelier2/src/main/java/com/sp/service/BuySellService.java
@@ -0,0 +1,32 @@
+package com.sp.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sp.service.CardService;
+import com.sp.service.UserService;
+
+@Service
+public class BuySellService{
+	
+	@Autowired
+	private CardService cService;
+	@Autowired
+	private UserService uService;
+	
+	
+	public void buyCard(int idc, int idu) {
+		int price = cService.getCard(idc).getPrice();
+		int userVault = uService.getArgent();
+		if(price > usreVault) {
+			return;
+		}
+		
+	}
+	
+	public void sellCard(int idc) {
+		
+	}
+}
