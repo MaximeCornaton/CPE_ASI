5a64d77f83729374fb179a7c17d504270b222177
rodologie <91213039+rodologie@users.noreply.github.com>
2023-05-05 12:01:14 +0200

    app

--- Atelier2/src/main/java/com/sp/SpAppSellBuy.java
+++ Atelier2/src/main/java/com/sp/SpAppSellBuy.java
@@ -2,8 +2,9 @@ package com.sp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+@Service
 public class SpAppSellBuy {
 	
 	public static void main(String[] args) {
