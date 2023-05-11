d4f145c656179297f6d99227e7734b5d9d61c4cf
Paul AUBRY <baekeland@outlook.fr>
2023-05-11 19:07:22 +0200

    sell OK

--- Atelier2/src/main/java/com/sp/controller/BuySellRestCrt.java
+++ Atelier2/src/main/java/com/sp/controller/BuySellRestCrt.java
@@ -1,6 +1,8 @@
   package com.sp.controller;
 
-  import org.springframework.beans.factory.annotation.Autowired;
+  import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
   import org.springframework.web.bind.annotation.PathVariable;
   import org.springframework.web.bind.annotation.RequestBody;
   import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,10 @@
 	  BuySellService bsService;
 	  
 	  @RequestMapping(method=RequestMethod.POST, value="/buy")
-	  public void buyCard(@RequestBody int idc, int idu) {
-		  bsService.buyCard(idc, idu);
+	  public void buyCard(@RequestBody Map<String, Object> cardPurchaseRequest) {
+	    int idc = (int) cardPurchaseRequest.get("idc");
+	    int idu = (int) cardPurchaseRequest.get("idu");
+		bsService.buyCard(idc, idu);
 	  }
 	  
 	  @RequestMapping(method=RequestMethod.POST, value="/sell")
