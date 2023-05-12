2f953bff92f2355c32b505e6494ff70c50472bef
valbrqd <valbrqd@gmail.com>
2023-05-12 11:12:07 +0200

    ok

---
+++ Atelier3/UserS/UserAPP/src/main/java/com/sp/controller/UserCrt.java
@@ -0,0 +1,42 @@
+package com.sp.controller;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sp.model.User;
+import com.sp.service.UserService;
+
+/**
+ * @author paulaubry
+ *
+ */
+@RestController
+public class UserCrt {
+	
+	@Autowired
+	private UserService uService;
+	
+	@RequestMapping(value = {"/register"}, method = RequestMethod.POST)
+	public void addUser(@RequestBody User user) {
+		uService.addUser(user);
+		
+	}
+	
+	@RequestMapping(value = {"/{id}/cards"}, method = RequestMethod.GET)
+	public Set<Card> getCards(@PathVariable int id) {
+		return uService.getCards(id);
+	}
+	
+	@RequestMapping(value = {"/user/{id}"}, method = RequestMethod.GET)
+	public User getUser(@PathVariable int id) {
+		return uService.getUser(id);
+	}
+	
+}
+
---
+++ Atelier3/UserS/UserAPP/src/main/java/com/sp/model/User.java
@@ -0,0 +1,117 @@
+/**
+ * 
+ */
+package com.sp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+/**
+ * @author paulaubry
+ *
+ */
+@Entity @Table(name="T_USER")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private int idUser;
+	private String name;
+	private String surname;
+	private String password;
+	private int money;
+	
+	
+	/**
+	 * 
+	 */
+	public User() {
+		
+	}
+		
+
+	public void addCard(Card card) {
+		this.cardList.add(card);
+	}
+	
+	public void delCard(Card card) {
+		this.cardList.remove(card);
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return idUser;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.idUser = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the money
+	 */
+	public int getMoney() {
+		return money;
+	}
+	/**
+	 * @param money the money to set
+	 */
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	@Override
+	public String toString() {
+		return "User [idUser=" + idUser + ", name=" + name + ", surname=" + surname + ", password=" + password
+				+ ", money=" + money + "]";
+	}
+
+}
