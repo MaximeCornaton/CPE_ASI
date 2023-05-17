b378590a6eb08ff1ee947de031cc00eb476d3978
Paul AUBRY <baekeland@outlook.fr>
2023-05-17 16:13:15 +0200

    Gitlab

---
+++ Atelier3/AuthentificationS/src/test/java/com/sp/controller/AuthRestCrtTest.java
@@ -0,0 +1,10 @@
+package com.sp.controller;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class 
